4747318c48c8b2b51d7d33beccb34b416ff3158c
Phidk <Phidkalt@gmail.com>
2024-04-30 13:45:22 +0200

    Update Controller.java

--- PRO5_Kas/src/controller/Controller.java
+++ PRO5_Kas/src/controller/Controller.java
@@ -4,45 +4,44 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Controller {
-
-    public void createKonference(String navn, String adresse, int konferenceAfgift, LocalDate startDato, LocalDate slutDato) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void createHotel(String navn, String adresse, int singlePris, int doublePris) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void createUdflugt(String navn, String destination, LocalDate dato, LocalDateTime tid, int pris, Boolean frokost) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void createDeltager(String navn, Boolean foredragsholder, String adresse, String land, String by, String tlfNr) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void createRegistration(String firmaTlfNr, String firmaNavn, LocalDate ankomstDato, LocalDate afskedsdato, HotelVærelse hotelRum, Deltager deltager, Konference konference) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void removeKonference(Konference konference) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void removeHotel(Hotel hotel) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void removeUdflugt(Udflugt udflugt) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void removeDeltager(Deltager deltager) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void removeRegistration(Registration registration) {
-        throw new UnsupportedOperationException();
-    }
+//    public void createKonference(String navn, String adresse, int konferenceAfgift, LocalDate startDato, LocalDate slutDato) {
+//        throw new UnsupportedOperationException();
+//    }
+//
+//    public void createHotel(String navn, String adresse, int singlePris, int doublePris) {
+//        throw new UnsupportedOperationException();
+//    }
+//
+//    public void createUdflugt(String navn, String destination, LocalDate dato, LocalDateTime tid, int pris, Boolean frokost) {
+//        throw new UnsupportedOperationException();
+//    }
+//
+//    public void createDeltager(String navn, Boolean foredragsholder, String adresse, String land, String by, String tlfNr) {
+//        throw new UnsupportedOperationException();
+//    }
+//
+//    public void createRegistration(String firmaTlfNr, String firmaNavn, LocalDate ankomstDato, LocalDate afskedsdato, Deltager deltager, Konference konference) {
+//        throw new UnsupportedOperationException();
+//    }
+//
+//    public void removeKonference(Konference konference) {
+//        throw new UnsupportedOperationException();
+//    }
+//
+//    public void removeHotel(Hotel hotel) {
+//        throw new UnsupportedOperationException();
+//    }
+//
+//    public void removeUdflugt(Udflugt udflugt) {
+//        throw new UnsupportedOperationException();
+//    }
+//
+//    public void removeDeltager(Deltager deltager) {
+//        throw new UnsupportedOperationException();
+//    }
+//
+//    public void removeRegistration(Registration registration) {
+//        throw new UnsupportedOperationException();
+//    }
 }
 
