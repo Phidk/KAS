219be8b10849a88c2020b03d125487b979b86db6
0VeRC0DeT <83798002+0VeRC0DeT@users.noreply.github.com>
2024-04-30 14:01:09 +0200

    Deltager klassen
    deltager klassen men ikke færdig, mangler ting

---
+++ PRO5_Kas/src/model/Deltager.java
@@ -0,0 +1,73 @@
+package model;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Deltager {
+
+    private String navn;
+
+    private String adresse;
+
+    boolean foredragsholder;
+
+    private String land;
+
+    private String by;
+
+    private String tlfNr;
+
+    private final ArrayList<Registration> registrationer = new ArrayList<Registration>();
+
+    public Deltager(String navn, String adresse, boolean foredragsholder, String land, String by, String tlfNr) {
+        this.navn = navn;
+        this.adresse = adresse;
+        this.foredragsholder = foredragsholder;
+        this.land = land;
+        this.by = by;
+        this.tlfNr = tlfNr;
+    }
+
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public boolean isForedragsholder() {
+        return foredragsholder;
+    }
+
+    public String getLand() {
+        return land;
+    }
+
+    public String getBy() {
+        return by;
+    }
+
+    public String getTlfNr() {
+        return tlfNr;
+    }
+
+    public ArrayList<Registration> getRegistrationer() {
+        return registrationer;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Deltager{" +
+                "navn='" + navn + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", foredragsholder=" + foredragsholder +
+                ", land='" + land + '\'' +
+                ", by='" + by + '\'' +
+                ", tlfNr='" + tlfNr + '\'' +
+                ", registrationer=" + registrationer +
+                '}';
+    }
+}
