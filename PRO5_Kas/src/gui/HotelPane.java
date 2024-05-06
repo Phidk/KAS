40ad168e10b4d1f6c6f5b75c1f6376ebf1afb62d
JosephineGormsen <josephinegormsen@hotmail.com>
2024-05-06 11:58:00 +0200

    StartWindow

---
+++ PRO5_Kas/src/gui/HotelPane.java
@@ -0,0 +1,22 @@
+package gui;
+
+ import model.*;
+ import controller.*;
+ import javafx.beans.value.ChangeListener;
+ import javafx.geometry.HPos;
+ import javafx.geometry.Insets;
+ import javafx.scene.control.*;
+ import javafx.scene.layout.GridPane;
+ import javafx.scene.layout.HBox;
+import java.util.ArrayList;
+
+public class HotelPane extends GridPane {
+   private Hotel hotel;
+   private Tillæg tillæg;
+
+   private final ListView<Hotel> lvwHotels;
+   private final ListView<Tillæg> lvwTillæg;
+
+
+
+}
