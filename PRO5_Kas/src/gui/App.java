2507724e183cf1a563586fb8f7c964e24d544666
JosephineGormsen <josephinegormsen@hotmail.com>
2024-05-07 22:56:46 +0200

    GUI

--- PRO5_Kas/src/gui/App.java
+++ PRO5_Kas/src/gui/App.java
@@ -14,15 +14,5 @@ public class App{
     public static void main(String[] args){
         Application.launch(StartWindow.class);
     }
-    public static void addClass (Node node, String strClass) {
-        ObservableList<String> styleClass = node.getStyleClass();
-        if (!styleClass.contains(strClass)) {
-            styleClass.add(strClass);
-        }
-    }
 
-    public static void removeClass (Node node, String strClass) {
-        ObservableList<String> styleClass = node.getStyleClass();
-        styleClass.remove(strClass);
-    }
 }
