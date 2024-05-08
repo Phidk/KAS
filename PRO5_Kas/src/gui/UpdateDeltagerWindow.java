d7dcb86902dfa4b9ce720597a422055d3e08aa71
JosephineGormsen <josephinegormsen@hotmail.com>
2024-05-08 10:31:42 +0200

    DeltagerPane færdig.ManglerRediger

---
+++ PRO5_Kas/src/gui/UpdateDeltagerWindow.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.StageStyle;
+import model.Deltager;
+
+
+public class UpdateDeltagerWindow extends Stage {
+
+    private Deltager deltager;
+
+    UpdateDeltagerWindow(Deltager deltager){
+
+    }
+
+}
